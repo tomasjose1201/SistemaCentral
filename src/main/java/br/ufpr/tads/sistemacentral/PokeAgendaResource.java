7fdf8f8633a0d4785253ee32f77d9182ca5bdc61
Tom <tomasjose1201@gmail.com>
2018-05-22 20:43:28 -0300

    Criação das Classes

--- src/main/java/br/ufpr/tads/sistemacentral/PokeAgendaResource.java
+++ src/main/java/br/ufpr/tads/sistemacentral/PokeAgendaResource.java
@@ -10,9 +10,11 @@ import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -25,30 +27,36 @@ public class PokeAgendaResource {
     @Context
     private UriInfo context;
 
-    /**
-     * Creates a new instance of PokeAgendaResource
-     */
     public PokeAgendaResource() {
     }
 
-    /**
-     * Retrieves representation of an instance of br.ufpr.tads.sistemacentral.PokeAgendaResource
-     * @return an instance of java.lang.String
-     */
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * PUT method for updating or creating an instance of PokeAgendaResource
-     * @param content representation for the resource
-     */
-    @PUT
+    //Serviços de autenticação (login e senha);
+    @POST
+    @Path("/autenticar")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    public Response autenticarTreinador() {
+        return Response.status(Response.Status.OK).build();
     }
     
+    //Serviço de cadastro de novo Pokemon;
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response cadastrarPokemon() {
+        return Response.status(Response.Status.OK).build();
+    }
+    
+    //Serviço de consulta de todos os Pokemons;
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getPokemons() {
+        throw new UnsupportedOperationException();
+    }
+    
+    //Serviço de pesquisa de um determinado Pokemon
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getPokemon() {
+        throw new UnsupportedOperationException();
+    }
 }
---
+++ src/main/java/br/ufpr/tads/sistemacentral/Treinador.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufpr.tads.sistemacentral;
+
+/**
+ *
+ * @author Tom
+ */
+public class Treinador {
+    private int idTreinador;
+    private String nomeTreinador;
+    private String login;
+    private String senha;
+
+    public Treinador() {
+    }
+
+    public int getIdTreinador() {
+        return idTreinador;
+    }
+
+    public void setIdTreinador(int idTreinador) {
+        this.idTreinador = idTreinador;
+    }
+
+    public String getNomeTreinador() {
+        return nomeTreinador;
+    }
+
+    public void setNomeTreinador(String nomeTreinador) {
+        this.nomeTreinador = nomeTreinador;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
