542cc35139be17b36bfb2bfe056a54ef2c2da87a
wesmaffazzolli <31376480+wesmaffazzolli@users.noreply.github.com>
2018-06-17 11:14:24 -0300

    ...

--- src/main/java/br/ufpr/tads/sistemacentral/PokeAgendaResource.java
+++ src/main/java/br/ufpr/tads/sistemacentral/PokeAgendaResource.java
@@ -70,10 +70,11 @@ public class PokeAgendaResource {
 
     //Serviço de consulta de todos os Pokemons;
     @GET
-    public Response getPokemons() throws SQLException {
+    public ArrayList<Pokemon> getPokemons() throws SQLException {
         SistemaCentralDao dao = new SistemaCentralDao();
         ArrayList<Pokemon> pokemons = dao.selectAll();
-        return Response.ok(pokemons).header("Access-Control-Allow-Origin", "*").build();
+        //return Response.ok(pokemons).header("Access-Control-Allow-Origin", "*").build();
+        return pokemons;
     }
 
     //Serviço de pesquisa de um determinado Pokemon
